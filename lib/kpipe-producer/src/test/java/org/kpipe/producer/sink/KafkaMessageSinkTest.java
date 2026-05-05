dcd8ac8760ad04b6ccc3690e025de0b0cf0a2cf4
mariano <mariano.gonzalez.mx@gmail.com>
2026-05-05 07:20:44 -0400

    refactor: improve error handling and logging in KafkaMessageSink and DefaultSink
     Conflicts:
    	lib/kpipe-producer/src/test/java/org/kpipe/producer/sink/KafkaMessageSinkTest.java

--- lib/kpipe-producer/src/test/java/org/kpipe/producer/sink/KafkaMessageSinkTest.java
+++ lib/kpipe-producer/src/test/java/org/kpipe/producer/sink/KafkaMessageSinkTest.java
@@ -6,7 +6,6 @@ import static org.mockito.Mockito.*;
 
 import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.Producer;
-import org.apache.kafka.clients.producer.ProducerRecord;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
